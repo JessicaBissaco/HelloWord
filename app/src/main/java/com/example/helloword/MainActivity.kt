000863db1b2d6b86efb5e35584e9fd9bb66ba200
User <jessica_miojinho@hotmail.com>
2022-03-08 10:07:15 -0300

    Comentário Alteração 1.1

--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.helloword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//Comentário Aletração 1.1
 class MainActivity : AppCompatActivity() {
    //Teste 1
     override fun onCreate(savedInstanceState: Bundle?) {
